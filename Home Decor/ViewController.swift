1d377007648211d2a5e95d28f6430dd3746789c7
Μιχάλης Λουκέρης <e4lm@hotmail.com>
2019-01-02 11:51:27 +0200

    Adding a plane node, with image as a material, in an ARPlaneAnchor

--- Home Decor/ViewController.swift
+++ Home Decor/ViewController.swift
@@ -18,15 +18,53 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        config.planeDetection = .horizontal
         sceneView.session.run(config)
         
+        sceneView.delegate = self
+        
+    }
+
+    func addCapsuleToTheScene() {
         let capsuleNode = SCNNode(geometry: SCNCapsule(capRadius: 0.03, height: 0.1))
         capsuleNode.position = SCNVector3(0.1, 0.1, -0.1)
         capsuleNode.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
         capsuleNode.eulerAngles = SCNVector3(0, 0, Double.pi/2)
         sceneView.scene.rootNode.addChildNode(capsuleNode)
     }
-
-
 }
 
+extension ViewController: ARSCNViewDelegate {
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else {
+            return
+        }
+        
+        node.enumerateChildNodes { (node, _) in
+            node.removeFromParentNode()
+        }
+        
+        let planeNode = createFloorNode(anchor: planeAnchor)
+        node.addChildNode(planeNode)
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
+        guard let _ = anchor as? ARPlaneAnchor else {
+            return
+        }
+        
+        node.enumerateChildNodes { (node, _) in
+            node.removeFromParentNode()
+        }
+    }
+    
+    
+    func createFloorNode(anchor: ARPlaneAnchor) -> SCNNode {
+        let floorNode = SCNNode(geometry: SCNPlane(width: CGFloat(anchor.extent.x), height: CGFloat(anchor.extent.z)))
+        floorNode.position = SCNVector3(anchor.center.x, 0, anchor.center.z)
+        floorNode.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "floor.jpg")
+        floorNode.geometry?.firstMaterial?.isDoubleSided = true
+        floorNode.eulerAngles = SCNVector3(Double.pi/2, 0, 0)
+        return floorNode
+    }
+}
